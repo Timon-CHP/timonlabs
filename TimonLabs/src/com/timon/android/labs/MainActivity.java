60ada3869eeb9a0a4989db1eea874c5cf7af383a
hungtrinh.t3h <hungtrinh.t3h@gmail.com>
2013-02-28 08:09:28 +0000

    ! Implement for validating and starting download.

--- TimonLabs/src/com/timon/android/labs/MainActivity.java
+++ TimonLabs/src/com/timon/android/labs/MainActivity.java
@@ -3,13 +3,26 @@ package com.timon.android.labs;
 import java.io.IOException;
 import java.util.Locale;
 
+import com.timon.android.labs.gpservices.GPDownloaderService;
 import com.timon.android.labs.utils.GKIMLog;
 
+import com.fedorvlasov.lazylist.LazyAdapter;
+import com.fedorvlasov.lazylist.LazyListMainActivity;
+import com.google.android.vending.expansion.downloader.DownloadProgressInfo;
+import com.google.android.vending.expansion.downloader.DownloaderClientMarshaller;
+import com.google.android.vending.expansion.downloader.DownloaderServiceMarshaller;
+import com.google.android.vending.expansion.downloader.IDownloaderClient;
+import com.google.android.vending.expansion.downloader.IDownloaderService;
+import com.google.android.vending.expansion.downloader.IStub;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.MediaPlayer;
@@ -17,16 +30,22 @@ import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Messenger;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.SurfaceHolder;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -129,10 +148,93 @@ public class MainActivity extends FragmentActivity {
 
 		PullToRefreshScrollView mPullRefreshScrollView;
 		ScrollView mScrollView;
+		public ProgressDialog mProgressDialog; 
+		private IStub mDownloaderClientStup;
+		private IDownloaderService mRemoteService = null;
+		private IDownloaderClient mDownloaderClient = new IDownloaderClient() {
+			
+			@Override
+			public void onServiceConnected(Messenger m) {
+				GKIMLog.lf(null, 1, TAG+"=>onServiceConnected: " + m.toString());
+				mRemoteService = DownloaderServiceMarshaller.CreateProxy(m);
+			    mRemoteService.onClientUpdated(mDownloaderClientStup.getMessenger());
+			}
+			
+			@Override
+			public void onDownloadStateChanged(int newState) {
+				GKIMLog.lf(null, 1, TAG+"=>onDownloadStateChanged: "+ newState);
+			}
+			
+			@Override
+			public void onDownloadProgress(DownloadProgressInfo progress) {
+				GKIMLog.lf(null, 1, TAG+"=>onDownloadProgress: "+ progress.toString());
+				
+			}
+		};
+		
+		public OnClickListener mOnClickToDownloadAsset = new OnClickListener() {
+			
 
+			@Override
+			public void onClick(View v) {
+				// TODO Implementing Downloading service
+				// Build an Intent to start this activity from the Notification
+				Intent notifierIntent = new Intent(getActivity(),
+						MainActivity.class);
+				notifierIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+						| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				PendingIntent pendingIntent = PendingIntent.getActivity(
+						getActivity(), 0, notifierIntent,
+						PendingIntent.FLAG_UPDATE_CURRENT);
+
+				// Start the download service (if required)
+				try {
+					int startResult = DownloaderClientMarshaller
+							.startDownloadServiceIfRequired(getActivity(),
+									pendingIntent, GPDownloaderService.class);
+					GKIMLog.lf(null, 0, TAG+"=>startDownloadServiceIfRequired.");
+					// If download has started, initialize this activity to show
+					// download progress
+					if (startResult != DownloaderClientMarshaller.NO_DOWNLOAD_REQUIRED) {
+						mDownloaderClientStup = DownloaderClientMarshaller.CreateStub(mDownloaderClient,
+				                GPDownloaderService.class);
+						// This is where you do set up to display the download
+						// progress (next step)
+						if (mProgressDialog != null && !mProgressDialog.isShowing()) {
+							mProgressDialog.show();
+						}
+						return;
+					} // If the download wasn't necessary, fall through to start the
+						// app
+				} catch (NameNotFoundException e) {
+					 GKIMLog.lf(null, 5, TAG+"=>onReceive NameNotFoundException:" + e.getMessage());
+				}
+
+			}
+		};
+
+		
 		private TextView mTextView;
 
 		private AssetFileDescriptor afd;
+
+		@Override
+		public void onResume() {
+			GKIMLog.lf(null, 1, TAG+"=>onResume");
+			if (mDownloaderClientStup != null) {
+				mDownloaderClientStup.connect(getActivity());
+			}
+			super.onResume();
+		}
+
+		@Override
+		public void onStop() {
+			GKIMLog.lf(null, 1, TAG+"=>onStop");
+			if (mDownloaderClientStup != null) {
+				mDownloaderClientStup.disconnect(getActivity());
+			}
+			super.onStop();
+		}
 
 		public DummySectionFragment() {
 		}
@@ -143,6 +245,7 @@ public class MainActivity extends FragmentActivity {
 			// Create a new TextView and set its text to the fragment's section
 			// number argument value.
 			GKIMLog.lf(null, 0, TAG + "=>onCreateView.");
+			mProgressDialog = new ProgressDialog(getActivity());
 			int number = 0;
 			if (savedInstanceState != null) {
 				if (savedInstanceState
@@ -168,6 +271,8 @@ public class MainActivity extends FragmentActivity {
 				root = initSecondFragVideoView(inflater);
 				break;
 			case 2:
+				root = initThirdFragList(inflater);
+				break;
 			case 0:
 			default:
 				root = inflater.inflate(R.layout.activity_ptr_scrollview, null,
@@ -209,6 +314,9 @@ public class MainActivity extends FragmentActivity {
 					.findViewById(R.id.tv_second_frag);
 			pathV.setText(path);
 
+			Button btnDownload = (Button) result
+					.findViewById(R.id.btn_second_frag);
+
 			AssetManager am = getActivity().getAssets();
 			if (am != null) {
 				try {
@@ -221,53 +329,65 @@ public class MainActivity extends FragmentActivity {
 									+ e.getMessage());
 				}
 			}
-
 			SurfaceHolder holder = videoV.getHolder();
 			final MediaPlayer mp = new MediaPlayer();
-			holder.addCallback(new SurfaceHolder.Callback() {
-				@Override
-				public void surfaceCreated(SurfaceHolder holder) {
-					GKIMLog.lf(null, 0, TAG + "=>surfaceCreated.");
-					try {
-						mp.setDisplay(holder);
-						mp.setDataSource(afd.getFileDescriptor(),
-								afd.getStartOffset(), afd.getLength());
-						mp.setLooping(true);
-						
-						mp.prepareAsync();
-					} catch (IllegalArgumentException e) {
-						GKIMLog.lf(
-								null,
-								5,
-								TAG
-										+ "=>initSecondFragVideoView IllegalArgumentException: "
-										+ e.getMessage());
-					} catch (IllegalStateException e) {
-						GKIMLog.lf(
-								null,
-								5,
-								TAG
-										+ "=>initSecondFragVideoView IllegalStateException: "
-										+ e.getMessage());
-					} catch (IOException e) {
-						GKIMLog.lf(null, 5,
-								TAG + "=>initSecondFragVideoView IOException: "
-										+ e.getMessage());
+			if (afd == null) {
+				GKIMLog.lf(null, 0, TAG + "=> Asset file is null");
+				if (btnDownload != null) {
+					btnDownload.setVisibility(View.VISIBLE);
+					btnDownload.setOnClickListener(mOnClickToDownloadAsset);
+				}
+
+			} else {
+				GKIMLog.lf(null, 0, TAG + "=> Asset file is existed");
+				if (btnDownload != null) {
+					btnDownload.setText("Already got Asset files");
+					btnDownload.setOnClickListener(null);
+				}
+				holder.addCallback(new SurfaceHolder.Callback() {
+					@Override
+					public void surfaceCreated(SurfaceHolder holder) {
+						GKIMLog.lf(null, 0, TAG + "=>surfaceCreated.");
+						try {
+							mp.setDisplay(holder);
+							mp.setDataSource(afd.getFileDescriptor(),
+									afd.getStartOffset(), afd.getLength());
+							mp.setLooping(true);
+
+							mp.prepareAsync();
+						} catch (IllegalArgumentException e) {
+							GKIMLog.lf(
+									null,
+									5,
+									TAG
+											+ "=>initSecondFragVideoView IllegalArgumentException: "
+											+ e.getMessage());
+						} catch (IllegalStateException e) {
+							GKIMLog.lf(
+									null,
+									5,
+									TAG
+											+ "=>initSecondFragVideoView IllegalStateException: "
+											+ e.getMessage());
+						} catch (IOException e) {
+							GKIMLog.lf(null, 5, TAG
+									+ "=>initSecondFragVideoView IOException: "
+									+ e.getMessage());
+						}
 					}
-				}
 
-				@Override
-				public void surfaceDestroyed(SurfaceHolder holder) {
-					GKIMLog.lf(null, 0, TAG + "=>surfaceDestroyed.");
-				}
+					@Override
+					public void surfaceDestroyed(SurfaceHolder holder) {
+						GKIMLog.lf(null, 0, TAG + "=>surfaceDestroyed.");
+					}
 
-				@Override
-				public void surfaceChanged(SurfaceHolder holder, int format,
-						int width, int height) {
-					GKIMLog.lf(null, 0, TAG
-							+ "=>surfaceChanged.");
-				}
-			});
+					@Override
+					public void surfaceChanged(SurfaceHolder holder,
+							int format, int width, int height) {
+						GKIMLog.lf(null, 0, TAG + "=>surfaceChanged.");
+					}
+				});
+			}
 			mp.setOnCompletionListener(new OnCompletionListener() {
 				public void onCompletion(MediaPlayer mp) {
 					GKIMLog.lf(null, 0,
@@ -285,6 +405,31 @@ public class MainActivity extends FragmentActivity {
 			});
 
 			GKIMLog.lf(null, 0, TAG + "=>initSecondFragVideoView done.");
+			return result;
+		}
+
+		private View initThirdFragList(LayoutInflater inflater) {
+			LinearLayout result = new LinearLayout(getActivity());
+			result.setOrientation(LinearLayout.VERTICAL);
+			result.setGravity(Gravity.CENTER);
+			OnClickListener l = new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					switch (v.getId()) {
+					case 0: // Open LazyList
+						startActivity(new Intent(getActivity(),
+								LazyListMainActivity.class));
+						break;
+					default:
+						break;
+					}
+				}
+			};
+			Button btn1 = new Button(getActivity());
+			btn1.setId(0);
+			btn1.setText("Open LazyList");
+
+			result.addView(btn1);
 			return result;
 		}
 
@@ -320,6 +465,17 @@ public class MainActivity extends FragmentActivity {
 				super.onPostExecute(result);
 			}
 		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
 	}
 
 }
